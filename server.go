7062523521f82ddefc15d9099503a9909909c528
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2013-05-21 08:53:25 -0700

    add some basic log output

--- server.go
+++ server.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"net/http"
+	"log"
 )
 
 func main() {
+	log.Println("Starting Server")
 	http.Handle("/", http.FileServer(http.Dir("./public/")))
+
+	log.Println("Listening on 8080")
 	http.ListenAndServe(":8080", nil)
 }

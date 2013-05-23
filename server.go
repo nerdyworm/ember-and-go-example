8c97aaa8106f055a5bcfa2a1a8a3b24e9d011aea
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2013-05-23 15:41:20 -0700

    simple server side

--- server.go
+++ server.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"log"
 	"github.com/gorilla/mux"
 )
+
+type Kitten struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Picture string `json:"picture"`
+}
+
+type KittenJSON struct {
+	Kitten Kitten `json:"kitten"`
+}
 
 func KittensHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -16,11 +27,35 @@ func KittensHandler(w http.ResponseWriter, r *http.Request) {
 	]}`))
 }
 
+func CreateKittenHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the incoming kitten from the request body
+	var kittenJSON KittenJSON
+	err := json.NewDecoder(r.Body).Decode(&kittenJSON)
+	if err != nil {
+		panic(err)
+	}
+
+	// Grab the kitten and set some dummy data
+	kitten := kittenJSON.Kitten
+	kitten.Id = 5
+	kitten.Picture = "http://placekitten.com/300/200"
+
+	// Serialize the modified kitten to JSON
+	j, err := json.Marshal(KittenJSON{Kitten: kitten})
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(j)
+}
+
 func main() {
 	log.Println("Starting Server")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/api/kittens", KittensHandler).Methods("GET")
+	r.HandleFunc("/api/kittens", CreateKittenHandler).Methods("POST")
 	http.Handle("/api/", r)
 
 	http.Handle("/", http.FileServer(http.Dir("./public/")))

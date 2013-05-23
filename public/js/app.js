762b44ff4536a152a7ddf97621b421145d8903ba
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2013-05-23 15:07:29 -0700

    client side code to create a kitten

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,10 @@
     })
   });
 
+  App.Router.map(function() {
+    this.route('create');
+  });
+
   App.Kitten = DS.Model.extend({
     name: DS.attr('string'),
     picture: DS.attr('string')
@@ -17,6 +21,19 @@
   App.IndexRoute = Ember.Route.extend({
     model: function() {
       return App.Kitten.find();
+    }
+  });
+
+  App.CreateController = Ember.Controller.extend({
+    name: null,
+    save: function() {
+      var kitten = App.Kitten.createRecord({
+        name: this.get('name')
+      });
+
+      kitten.save().then(function() {
+        this.transitionToRoute('index');
+      }.bind(this));
     }
   });
 })();

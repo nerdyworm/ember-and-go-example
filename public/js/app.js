377a560f3f98000a468e29851e4f7c7773e9b18d
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2013-05-27 11:37:01 -0700

    delete and update kittens

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 (function() {
 
-  var App = Ember.Application.create();
+  var App = window.App = Ember.Application.create();
 
   DS.Store.create({
     revision: 12,
@@ -11,6 +11,7 @@
 
   App.Router.map(function() {
     this.route('create');
+    this.route('edit', {path: '/edit/:kitten_id'});
   });
 
   App.Kitten = DS.Model.extend({
@@ -21,6 +22,13 @@
   App.IndexRoute = Ember.Route.extend({
     model: function() {
       return App.Kitten.find();
+    },
+
+    events: {
+      deleteKitten: function(kitten) {
+        kitten.deleteRecord();
+        kitten.save();
+      }
     }
   });
 
@@ -34,6 +42,15 @@
       kitten.save().then(function() {
         this.transitionToRoute('index');
         this.set('name', '');
+      }.bind(this));
+    }
+  });
+
+  App.EditController = Ember.ObjectController.extend({
+    save: function() {
+      var kitten = this.get('model');
+      kitten.save().then(function() {
+        this.transitionToRoute('index');
       }.bind(this));
     }
   });
--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"log"
 	"github.com/gorilla/mux"
+	"strconv"
 )
 
 type Kitten struct {
@@ -21,6 +22,7 @@ type KittensJSON struct {
 	Kittens []Kitten `json:"kittens"`
 }
 
+var idCounter int
 var kittens []Kitten
 
 func KittensHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,9 +42,11 @@ func CreateKittenHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
+	idCounter++
+
 	// Grab the kitten and set some dummy data
 	kitten := kittenJSON.Kitten
-	kitten.Id = len(kittens) + 1
+	kitten.Id = idCounter
 	kitten.Picture = "http://placekitten.com/300/200"
 
 	kittens = append(kittens, kitten)
@@ -57,12 +61,66 @@ func CreateKittenHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(j)
 }
 
+func UpdateKittenHandler(w http.ResponseWriter, r *http.Request) {
+	// Grab the kitten's id from the incoming url
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		panic(err)
+	}
+
+	// Decode the incoming kitten json
+	var kittenJSON KittenJSON
+	err = json.NewDecoder(r.Body).Decode(&kittenJSON)
+	if err != nil {
+		panic(err)
+	}
+
+	// Find the kitten in our kittens slice and upate it's name
+	for index, _ := range kittens {
+		if kittens[index].Id == id {
+			kittens[index].Name = kittenJSON.Kitten.Name
+		}
+	}
+
+	// Respond with a 204 indicating success, but no content
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func DeleteKittenHandler(w http.ResponseWriter, r *http.Request) {
+	// Grab the kitten's id from the incoming url
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		panic(err)
+	}
+
+	// Find the index of the kitten
+	kittenIndex := -1
+	for index, _ := range kittens {
+		if kittens[index].Id == id {
+			kittenIndex = index
+			break
+		}
+	}
+
+	// If we actually found a kitten remove it from the slice
+	if kittenIndex != -1 {
+		kittens = append(kittens[:kittenIndex], kittens[kittenIndex+1:]...)
+	}
+
+	// Respond with a 204 indicating success, but no content
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
 	log.Println("Starting Server")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/api/kittens", KittensHandler).Methods("GET")
 	r.HandleFunc("/api/kittens", CreateKittenHandler).Methods("POST")
+	r.HandleFunc("/api/kittens/{id}", UpdateKittenHandler).Methods("PUT")
+	r.HandleFunc("/api/kittens/{id}", DeleteKittenHandler).Methods("DELETE")
 	http.Handle("/api/", r)
 
 	http.Handle("/", http.FileServer(http.Dir("./public/")))
